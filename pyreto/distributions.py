9752ab62281c1f28ec87cc1c917970e094cce62e
David R. Pugh <drobert.pugh@gmail.com>
2016-03-14 22:07:05 +0000

    Added approximation for discrete MLE; goodness of fit testing procedure.

--- pyreto/distributions.py
+++ pyreto/distributions.py
@@ -1,5 +1,6 @@
 import numpy as np
-from scipy import integrate, optimize, special
+import pandas as pd
+from scipy import integrate, optimize, special, stats
 import statsmodels as sm
 
 from . import results
@@ -25,6 +26,11 @@ class Distribution:
     def _normalization_constant(xmin, *args, **kwargs):
         raise NotImplementedError
 
+    @staticmethod
+    def _quantile(x, xmin, *args, **kwargs):
+        """Inverse of the cumulative distibution function (CDF)."""
+        raise NotImplementedError
+
     @classmethod
     def _log_likelihood(cls, x, xmin, *args, **kwargs):
         """Pointwise log-likelihood function."""
@@ -42,6 +48,25 @@ class Distribution:
         C = cls._normalization_constant(x, *args, **params)
         f = cls._density_function(xmin, *args, **params)
         return C * f
+
+    @classmethod
+    def _rvs(cls, random_state, N, xmin, *args, **kwargs):
+        sample = stats.uniform.rvs(0, 1, size=N, random_state=random_state)
+        return cls._quantile(sample, xmin, *args, **kwargs)
+
+    @classmethod
+    def _generate_synthetic_data(cls, prng, clean_data, result):
+        N = clean_data.count()
+        body = clean_data[clean_data < result.xmin]
+        body_prob = body.count() / N
+        successes = stats.binom.rvs(1, body_prob, size=N, random_state=prng)
+        n_body = np.sum(successes)
+        body_sample = prng.choice(body, n_body)
+        n_tail = N - n_body
+        tail_sample = cls._rvs(prng, n_tail, result.xmin, **result.params)
+        synthetic_data = pd.Series(np.hstack((body_sample, tail_sample)),
+                                   index=clean_data.index)
+        return synthetic_data
 
 
 class Exponential(Distribution):
@@ -131,6 +156,22 @@ class Pareto(Distribution):
 
         return result
 
+    @classmethod
+    def test_goodness_of_fit(cls, seed, result, data, xmin=None,
+                             quantile=0.99, discrete=False, approx=False,
+                             method='brute', solver_opts=None,
+                             replications=1000):
+        prng = np.random.RandomState(seed)
+        ks_distances = np.empty(replications)
+        clean_data = cls._clean_data(data)
+        for i in range(replications):
+            tmp_data = cls._generate_synthetic_data(prng, clean_data, result)
+            tmp_result = cls.fit(tmp_data, xmin, quantile, discrete, approx,
+                                 method)
+            ks_distances[i] = tmp_result.D
+        pvalue = ks_distances[ks_distances <= result.D].mean()
+        return pvalue, ks_distances
+
     @staticmethod
     def _cdf(x, xmin, alpha):
         """Cumulative distribution function (CDF)."""
@@ -171,6 +212,11 @@ class Pareto(Distribution):
     @staticmethod
     def _normalization_constant(xmin, alpha):
         return (alpha - 1) * xmin**(alpha - 1)
+
+    @staticmethod
+    def _quantile(q, xmin, alpha):
+        """Inverse of the cumulative distibution function (CDF)."""
+        return xmin / (1 - q)**(1 / (alpha - 1))
 
     @classmethod
     def _brute_force_minimize(cls, xmins, clean_data, discrete, approx):
